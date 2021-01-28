664f015a68f54992cac3bb8270af2ef3c2a53800
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-28 15:13:45 -0500

    Added Paint, rims, and sound system classes

---
+++ decoratordesignpattern/Rims.java
@@ -0,0 +1,23 @@
+package decoratordesignpattern;
+
+/**
+ * 
+ * @author Rachael
+ */
+public class Rims extends VehicleDecorator {
+
+    private Vehicle vehicle;
+
+    public Rims(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+    
+    public String toString() {
+        return vehicle.toString() + "cool rims";
+    }
+
+    public double getCost() {
+        return vehicle.getCost() + 200;
+    }
+    
+}
---
+++ decoratordesignpattern/SoundSystem.java
@@ -0,0 +1,23 @@
+package decoratordesignpattern;
+
+/**
+ * 
+ * @author Rachael
+ */
+public class SoundSystem extends VehicleDecorator {
+
+    private Vehicle vehicle;
+
+    public SoundSystem(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+    
+    public String toString() {
+        return vehicle.toString() + "awesome sound";
+    }
+
+    public double getCost() {
+        return vehicle.getCost() + 350;
+    }
+    
+}
