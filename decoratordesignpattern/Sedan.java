e9ad3abab305f97aee33ffcf4cea4918dfdab736
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-28 14:07:07 -0500

    Added Sedan class

---
+++ decoratordesignpattern/Sedan.java
@@ -0,0 +1,23 @@
+package decoratordesignpattern;
+
+/**
+ * The sedan class is a child class of Vehicle that sets a description and returns its cost
+ * @author Rachael
+ */
+public class Sedan extends Vehicle{
+
+    /**
+     * constructor method that sets the description to "Sedan"
+     */
+    public Sedan() {
+        this.description = "Sedan";
+    }
+
+    /**
+     * method that returns the cost of the sedan
+     * @return double cost of the sedan
+     */
+    public double getCost() {
+        return 20000;
+    }
+}
